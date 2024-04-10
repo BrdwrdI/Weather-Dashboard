6e4b5b88ed9d7128060aa51a4d87bb5fca4bbda8
WRD <dandobilly@gmail.com>
2024-04-09 21:58:41 -0400

    added displayToday function

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,26 @@
-function displayToday() {
-    ;
+function displayToday(data) {
+    const box = document.getElementById('today-box');
+    box.innerHTML = '';
+
+    const tempText = document.getElementById('city-temp');
+    const windText = document.getElementById('city-wind');
+    const humidityText = document.getElementById('city-humidity');
+    const cityText = document.getElementById('city-name');
+
+    tempText.textContent = data.main.temp.toFixed() + '°F';
+    windText.textContent = data.wind.speed.toFixed() + 'MPH';
+    humidityText.textContent = data.main.humidity + '%';
+
+    const date = new Date(data.dt * 1000);
+    const formatDate = date.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit'
+    });
+
+    cityText.textContent = data.name + ' ' + formatDate;
+
+    const img = document.createElement('img');
+    img.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    box.appendChild(img);
 }
